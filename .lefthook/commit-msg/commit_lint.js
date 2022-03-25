a042719fb733ea8db10d40415b25c9e954b146d3
pham minh hai au <phamau1994x@gmail.com>
2022-03-26 00:08:41 +0700

    test: test prevent hook commit 2

--- .lefthook/commit-msg/commit_lint.js
+++ .lefthook/commit-msg/commit_lint.js
@@ -5,7 +5,6 @@ const log = console.log;
 const {argv} = process;
 const pathFileParameter = argv[2];
 const commitMessageStr = shell.cat(pathFileParameter);
-console.log(`content commit in hook commit-msg: ${commitMessageStr.toString()}`);
 const reg = new RegExp(/^(feat|build|chore|ci|docs|fix|perf|refactor|revert|style|test):.+/, 'gm');
 if(reg.test(commitMessageStr.toString())) {
     log(chalk.green('commit message is valid'))
---
+++ .lefthook/pre-commit/check_skip_commit.js
@@ -0,0 +1,8 @@
+import shell from 'shelljs';
+import chalk from 'chalk';
+
+const log = console.log;
+const {argv} = process;
+const pathFileParameter = argv[2];
+const commitMessageStr = shell.cat(pathFileParameter);
+console.log(`content commit in hook pre-commit: ${commitMessageStr.toString()}`);
